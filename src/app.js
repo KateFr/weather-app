22107e7976112843ff99ed191b8ba47bd7e2eba1
Katarzyna Frasunkiewicz <katarzyna.frasunkiewicz@atos.net>
2020-04-05 19:09:43 +0200

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const geocode = require('./utils/geocode')
 
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // define path paths for express config
 const publicDir = path.join(__dirname, '../public')
@@ -78,6 +79,6 @@ app.get('*', (req, res) => {
    })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log('Server is up on port '+ port)
 })
